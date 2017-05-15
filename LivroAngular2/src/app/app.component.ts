afbc5b405bc3f5d1658f5d0a05574f0f26ea7016
Paulo Eduardo Fagundes Santos <paulo.esantos@gmail.com>
2017-05-15 17:01:28 -0300

    'Incial'

---
+++ LivroAngular2/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '	Livro	Angular	2!';
+  foto: string = 'favicon.ico';
+
+  msgAlerta(): void {
+    alert('Livro Angular 2');
+  }
+}
---
+++ LivroAngular2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaPessoaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
